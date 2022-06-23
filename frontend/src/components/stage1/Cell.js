d747b10bf598c2699e1a67419fec10c128434a3b
edkim3275 <edkim3275@gmail.com>
2022-06-23 17:08:56 +0900

    stage1 기본 맵 UI

---
+++ frontend/src/components/stage1/Cell.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Cell = ({ type }) => {
+  return (
+    <div class="cell-wrapper">
+      {/* <div style={{width:50, height:50, backgroundColor:"#aad8ff"}}>cell</div> */}
+      <div class="cell"></div>
+    </div>
+  )
+}
+
+export default Cell;
--- frontend/src/pages/stage1.jsx
+++ frontend/src/pages/stage1.jsx
@@ -1,11 +1,36 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Cell from "../components/stage1/Cell";
+
+const createMap = () => {
+  return(
+    Array.from(Array(11), () => 
+      new Array(11).fill([0])
+    )
+  )
+}
+
+const Map = () => {
+  return (
+    <div class="map-wrapper">
+      {createMap().map(row => row.map((cell, x) => <Cell key={x} type={cell[0]}/>))}
+    </div>
+  )
+}
 
 function Stage1() {
+
   return (
-    <div>
+    <div class="stage1-wrapper">
       <h2>welcom to stage1</h2>
-      <Link to="/">home</Link>
+      <div class="stage1-container">
+        <aside>
+          <div>
+            <Link to="/">home</Link>
+          </div>
+        </aside>
+        <Map />
+      </div>
     </div>
   )
 }
