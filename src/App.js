299ab66e2ba33b52e26a42d723a19fe1dd027b9c
kyungilcho <kyungilch@gmail.com>
2022-06-24 10:30:00 +0900

    router 별도로 정리

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './styles/App.css';
+import Router from './components/Router';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Router />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+
+            <header >
+                <h2>hi</h2>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
