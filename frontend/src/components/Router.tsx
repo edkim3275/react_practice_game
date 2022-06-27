e5e6e69e0b3efdd196e8006db98f9549bd405d90
kyungilcho <kyungilch@gmail.com>
2022-06-27 16:10:01 +0900

    selector 이름 변경

--- frontend/src/components/Router.tsx
+++ frontend/src/components/Router.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../pages/main';
 import Stage1 from '../pages/stage1';
-import StageSelect from '../pages/StageSelector';
+import StageSelector from '../pages/StageSelector';
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={< Main />} />
-                < Route path="/stage" element={< StageSelect />} >
+                < Route path="/stage" element={< StageSelector />} >
                 </Route>
                 <Route path="stage/1" element={< Stage1 />} />
                 <Route path="*" element={<div>error</div>} />
--- frontend/src/pages/StageSelector.tsx
+++ frontend/src/pages/StageSelector.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function StageSelect() {
-    return (
-        <div>
-            <h1>stage select</h1>
-            <ul>
-                <li>
-                    <Link to="1">stage1</Link>
-                </li>
-            </ul>
-        </div>
-    )
+const StageSelector: React.FC = () => {
+  return (
+    <div>
+      <h1>stage select</h1>
+      <ul>
+        <li>
+          <Link to="1">stage1</Link>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
-export default StageSelect;
+export default StageSelector;
