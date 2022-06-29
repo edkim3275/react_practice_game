525831e9e0b8fee03312de4a651f4be29a003ca6
edkim3275 <edkim3275@gmail.com>
2022-06-29 16:21:57 +0900

    preventDefault 수정

--- frontend/src/utils/Movement.tsx
+++ frontend/src/utils/Movement.tsx
@@ -14,18 +14,21 @@ export function useKey(cb: Function) {
 
     useEffect(() => {
         function handle(event: KeyboardEvent) {
-            event.preventDefault();
             switch (event.code) {
                 case 'ArrowDown':
+                    event.preventDefault();
                     callbackRef.current((state: { x: number, y: number }) => ({ ...state, y: state.y + 5 }));
                     break;
                 case 'ArrowUp':
+                    event.preventDefault();
                     callbackRef.current((state: { x: number, y: number }) => ({ ...state, y: state.y - 5 }));
                     break;
                 case 'ArrowRight':
+                    event.preventDefault();
                     callbackRef.current((state: { x: number, y: number }) => ({ ...state, x: state.x + 5 }));
                     break;
                 case 'ArrowLeft':
+                    event.preventDefault();
                     callbackRef.current((state: { x: number, y: number }) => ({ ...state, x: state.x - 5 }));
                     break;
                 default:
